e3b9ed2c9d56f8ee5df4bb9190313e6e3bf0ec3e
Keeze <kees@vanhoekelen.nl>
2024-06-09 04:07:15 +0200

    modified:   config_buttons_example.h

--- config_buttons_example.h
+++ config_buttons_example.h
@@ -189,8 +189,8 @@
 // #define ROW_NUM     4
 // #define COLUMN_NUM  4
 // #define KEYPAD_KEYS {'1', '2', '3', 'A'},  {'4', '5', '6', 'B'},  {'7', '8', '9', 'C'},  {'*', '0', '#', 'D'}
-// #define KEYPAD_ROW_PINS    {19, 18, 5, 17}
-// #define KEYPAD_COLUMN_PINS {16, 4, 2, 15}
+// #define KEYPAD_ROW_PINS    {19, 18, 17, 16}
+// #define KEYPAD_COLUMN_PINS {4, 0, 2, 33}
 
 // 4x3 keypad - default
 // #define ROW_NUM     4
